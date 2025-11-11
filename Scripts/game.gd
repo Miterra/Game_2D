84f9614143899194deec79bcd6668c64cf1349d3
Miterra <halil.ostwald4@gmail.com>
2025-11-11 19:20:43 +0100

    ajout des panels

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+@onready var bat_principal = $bat_PrincipalWindows
 
 func _on_revenir_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+
+func _ready() -> void:
+	bat_principal.visible = false
+
+
+func _on_bat_principal_pressed() -> void:
+	bat_principal.visible = true
